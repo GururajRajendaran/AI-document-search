4f92e358bafa9c4a79050fa07eaba7170b84e28e
gowtham <gowthamgk24112002@gmail.com>
2026-06-09 10:55:59 +0530

    Added Streamlit secrets support

--- utils/gemini_helper.py
+++ utils/gemini_helper.py
@@ -1,11 +1,17 @@
 import os
+import streamlit as st
 from google import genai
 from dotenv import load_dotenv
 
 load_dotenv()
 
+api_key = os.getenv("GOOGLE_API_KEY")
+
+if not api_key:
+    api_key = st.secrets["GOOGLE_API_KEY"]
+
 client = genai.Client(
-    api_key=os.getenv("GOOGLE_API_KEY")
+    api_key=api_key
 )
 
 def ask_gemini(question, context):
